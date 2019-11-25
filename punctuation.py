ab206ad4e49b5f470ec0ad127a124b2f2cf16004
Dhananjay49 <44177299+Dhananjay49@users.noreply.github.com>
2019-11-25 10:11:00 +0530

    Add files via upload

---
+++ punctuation.py
@@ -0,0 +1,55 @@
+import numpy as np 
+import urllib
+import time
+import re
+import csv
+import nltk
+from nltk.corpus import stopwords ,wordnet
+from nltk.tokenize import word_tokenize, sent_tokenize 
+import itertools
+import requests
+import json
+from nltk.stem.wordnet import WordNetLemmatizer as wn
+
+def correct_sentence(initial):
+	s = '';
+	wordlist = nltk.word_tokenize(initial);
+	j=0;
+	ques_occur = False;
+	another_ques_occur = False;
+	x=0;
+	ques = False;
+	for word in wordlist:
+		words = nltk.word_tokenize(word)
+		tag = nltk.pos_tag(words)
+		print(tag)
+		print(word)
+		if(ques_occur==True and (word=='is' or word=='are' or word=='am') and j==x+1):
+			ques=True;
+			# print("it is a question")
+		if(word=='what' or word=='where' or word=='which' or word == 'who' or word=='how' or word == 'whose' or word=='when'):
+			# print("yes found a question")
+			ques_occur=True
+			x=j;
+		if(j==0 and (word=='is' or word=='are' or word=='do' or word=='does' or word=='has' or word=='have')):
+			ques=True;
+		if(tag[0][0]=='NNP' or j==0 or word=='i' or tag[0][0]=='NNPS'):
+			word = word.capitalize()
+		if(ques==True):
+			if(word=='.' or word=='?'):
+				s = s+'?'
+		if(j==0):
+			s = s+word
+		elif(not(word=='.'or word=='?')):
+			s = s+' '+word
+		j=j+1
+	return s
+
+
+p = input()
+s = p.lower()
+s1 = correct_sentence(s)
+correction = {}
+
+print(s1)
+
